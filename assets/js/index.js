22b5d26dcb5e5c58ffdd971b74d04fc532d0b91c
fanmiao11 <miao623@outlook.com>
2022-06-09 20:32:29 +0800

    完善了index.html

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers : {
+    //     Authorization : localStorage.getItem('token')
+    // },
+    success: (res) => {
+      // console.log(res);
+      if (res.status !== 0) return layer.msg(res.message);
+
+      layer.msg('获取用户信息成功！');
+
+      // 调用渲染函数
+      renderAvator(res.data);
+    },
+
+  });
+}
+// 渲染用户信息
+
+const renderAvator = (user) => {
+  const name = user.nickname || user.username;
+
+  $('#welcome').html(`欢迎${name}`);
+
+  let inner=''
+
+  if (user.user_pic !== null) {
+    // $('.layui-nav-img').attr('src',user.user_pic).show()
+    // $('.text-avatar').hide()
+
+    inner = ` <img src="${user.user_pic}" class="layui-nav-img" /> `;
+
+    $('.userinfo').prepend($(inner));
+  } else {
+
+    // $('.layui-nav-img').hide()
+    // let first = name[0].toUpperCase()
+    // $('.text-avatar').html(first).show()
+
+    inner = `<span class="text-avatar">${name[0].toUpperCase()}</span> `;
+  }
+$('.userinfo').prepend($(inner));
+  //     $('.userinfo').html(inner)
+};
+
+// 获取用户列表
+getUserInfo();
+
+// 退出登录
+
+$('#btnLogout').click(() => {
+
+  layer.confirm('是否退出登录？', { icon: 3, title: '提示' }, function (index) {
+    localStorage.removeItem('token');
+    location.href = '/login.html';
+  });
+
+});
+
